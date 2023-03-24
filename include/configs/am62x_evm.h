phytec/u-boot-phytec-ti

commit fd6ce7442eda23d508d5834bc0be7a5728c64b8c

configs: am62xx_evm: Drop EXTRA_ENV_DFUARGS for R5 SPL
Drop EXTRA_ENV_DFUARGS from R5 SPL build to save SRAM
space. There is no need for DFU args at R5 SPL stage.

Signed-off-by: Nitin Yadav <n-yadav@ti.com>

include/configs/am62x_evm.h
@@ -429,12 +429,16 @@
 
 #endif
 
+#ifdef CONFIG_TARGET_AM625_A53_EVM
 #define EXTRA_ENV_DFUARGS \
 	DFU_ALT_INFO_MMC \
 	DFU_ALT_INFO_EMMC \
 	DFU_ALT_INFO_RAM \
 	DFU_ALT_INFO_OSPI \
 	DFU_ALT_INFO_OSPI_NAND
+#else
+#define EXTRA_ENV_DFUARGS
+#endif
 
 /* Incorporate settings into the U-Boot environment */
 #define CONFIG_EXTRA_ENV_SETTINGS					\

cshilwant/ti-u-boot

commit 28c75c2713915a122e5d39311ae7416ca35c0532

configs: am62xx_evm: fix USB DFU boot mode for R5 SPL.
DFU boot mode is currently broken as EXTRA_ENV_DFUARGS
are excluded from R5 SPL build. Fix this by enabling
EXTRA_ENV_DFUARGS for R5 SPL conditional to CONFIG_SPL_DFU
also being enabled

Fixes: fd6ce7442ed (configs: am62xx_evm: Drop EXTRA_ENV_DFUARGS for R5 SPL)

Reported-by: Andreas Dannenberg <dannenberg@ti.com>
Signed-off-by: Nitin Yadav <n-yadav@ti.com>

include/configs/am62x_evm.h
@@ -504,7 +504,7 @@
 
 #endif
 
-#ifdef CONFIG_TARGET_AM625_A53_EVM
+#if defined(CONFIG_TARGET_AM625_A53_EVM) || defined(CONFIG_SPL_DFU)
 #define EXTRA_ENV_DFUARGS \
 	DFU_ALT_INFO_MMC \
 	DFU_ALT_INFO_EMMC \